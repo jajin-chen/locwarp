7427481c0e635088925c1c8b776f6d24242ae236
keezxc1223 <keezxc1223@users.noreply.github.com>
2026-05-12 09:08:15 +0800

    v0.2.148: 地址搜尋 (免費 Nominatim) 修復, 舊版 User-Agent 被 OSM 封鎖, 改成帶版號跟聯絡網址的字串

--- backend/config.py
+++ backend/config.py
@@ -39,7 +39,7 @@ BROUTER_BASE_URL = "https://brouter.de"
 
 # Nominatim
 NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org"
-NOMINATIM_USER_AGENT = "LocWarp/0.1"
+NOMINATIM_USER_AGENT = "LocWarp/0.2.148 (+https://github.com/keezxc1223/locwarp)"
 
 
 class SpeedProfile(TypedDict):
