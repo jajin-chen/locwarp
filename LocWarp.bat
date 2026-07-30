fb99bdf6f7e63a51c268dc9d43ebc2809f821328
smilecat772 <smilecat772@gmail.com>
2026-07-30 08:14:29 +0800

    fix: LocWarp.bat 改用 %LOCALAPPDATA% 取代寫死的使用者路徑

--- LocWarp.bat
+++ LocWarp.bat
@@ -15,5 +15,5 @@ exit /b
 :RUN
 cd /d "%~dp0"
 :: Use the full path to avoid the Windows Store python stub
-"C:\Users\USER\AppData\Local\Programs\Python\Python313\python.exe" start.py
+"%LOCALAPPDATA%\Programs\Python\Python313\python.exe" start.py
 pause
